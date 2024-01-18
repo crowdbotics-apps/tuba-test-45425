8ab5ff1a6c0a8a163eb46ebdf43ccf2f2b7cfccc
Crowdbotics <team@crowdbotics.com>
2024-01-18 07:38:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tuba-test-45425",
+  url: "https://tuba-test-45425.botics.co",
+  api: "https://tuba-test-45425.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
